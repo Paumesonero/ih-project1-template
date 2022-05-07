b53248343ac93e7a7576fec81262f76b65be0f84
Pau Mesonero <paumesonero@Caitlins-MacBook-Air.local>
2022-05-07 23:32:06 +0200

    getting started

--- js/game.js
+++ js/game.js
@@ -1,25 +1,30 @@
-class Game{
+class Game {
   constructor(context) {
     this.ctx = context;
+    this.spaceShip = new Player(300, 400, 40, 40);
   }
 
-  _assignControls() {
-    // Controles del teclado
-    document.addEventListener('keydown', (event) => {
-      switch (event.code) {
-        case 'ArrowLeft':
-          this.meatball.moveLeft();
-          break;
-        case 'ArrowRight':
-          this.meatball.moveRight();
-          break;
-        default:
-          break;
-      }
-    });
-  }
+  // _assignControls() {
+  //   // Controles del teclado
+  //   document.addEventListener('keydown', (event) => {
+  //     switch (event.code) {
+  //       case 'ArrowLeft':
+  //         this.meatball.moveLeft();
+  //         break;
+  //       case 'ArrowRight':
+  //         this.meatball.moveRight();
+  //         break;
+  //       default:
+  //         break;
+  //     }
+  //   });
+  // }
 
+  _drawSpaceShip() {
+    this.ctx.fillRect(this.spaceShip.x, this.spaceShip.y, this.spaceShip.width, this.spaceShip.height)
+  }
   _update() {
+    this._drawSpaceShip();
     window.requestAnimationFrame(() => this._update());
   }
 
