9c039b810f7fc6955bd4ed769c933d98e77d794a
Pau Mesonero <paumesonero@Paus-Mac-mini.local>
2022-05-08 00:06:53 +0200

    ball painted in center

--- js/game.js
+++ js/game.js
@@ -1,27 +1,32 @@
 class Game {
-  constructor(context) {
-    this.ctx = context;
-    this.spaceShip = new Player(300, 400, 40, 40);
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.spaceShip = new Player(500, 260, 40, 0, 2 * Math.PI);
   }
 
-  // _assignControls() {
-  //   // Controles del teclado
-  //   document.addEventListener('keydown', (event) => {
-  //     switch (event.code) {
-  //       case 'ArrowLeft':
-  //         this.meatball.moveLeft();
-  //         break;
-  //       case 'ArrowRight':
-  //         this.meatball.moveRight();
-  //         break;
-  //       default:
-  //         break;
-  //     }
-  //   });
-  // }
+  _assignControls() {
+    // Controles del teclado
+    document.addEventListener('keydown', (event) => {
+      switch (event.code) {
+        case 'ArrowLeft':
+          this.meatball.moveLeft();
+          break;
+        case 'ArrowRight':
+          this.meatball.moveRight();
+          break;
+        default:
+          break;
+      }
+    });
+  }
 
   _drawSpaceShip() {
-    this.ctx.fillRect(this.spaceShip.x, this.spaceShip.y, this.spaceShip.width, this.spaceShip.height)
+    this.ctx.beginPath()
+    this.ctx.arc(this.spaceShip.x, this.spaceShip.y, this.spaceShip.radius, this.spaceShip.startinstartingAngle, this.spaceShip.finishingAngle)
+    this.ctx.fillStyle = 'green'
+    this.ctx.fill()
+    // this.ctx.fillStyle = "orange"
+    // this.ctx.fillRect(this.spaceShip.x, this.spaceShip.y, this.spaceShip.width, this.spaceShip.height)
   }
   _update() {
     this._drawSpaceShip();
---
+++ js/player.js
@@ -0,0 +1,17 @@
+class Player {
+    constructor(x, y, radius, startingAngle, finishingAngle) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.startinstartingAngle = startingAngle;
+        this.finishingAngle = finishingAngle;
+    }
+
+    shoot() {
+
+    }
+
+    move() {
+
+    }
+}
